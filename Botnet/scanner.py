9d205ae59ade90cb0df83ae599d8d839afa0776e
root <root@decom-OptiPlex-780>
2018-04-09 14:49:09 -0300

    cefet

--- Botnet/scanner.py
+++ Botnet/scanner.py
@@ -41,29 +41,76 @@ def maskConverter(s):
         "255.255.255.255" : "/32"
     }.get(s, '/16')    
 
+def broadcast2Mask(s):
+	last = 0
+	points = 0
+	r = ''
+	for i in range(0,len(s)):
+		if s[i] == '.' or points == 3:
+			if s[last : i] == '255':
+				r = r + 0
+				points += 1
+				while points < 3:
+					r = r + '.0'
+					points += 1
+				return r
+			elif points == 3:
+				return r + '0'
+			else:
+				r = r + '255.'
+				last = i + 1
+				points += 1
+
+
+def broadcast2Ip(s, mask):
+	last = 0
+	points = 0
+	for i in range(0,len(s)):
+		if s[i] == '.' or points == 3:
+			if s[last : i] == '255':
+				r = s[0 : last - 1]
+				while points < 3:
+					r = r + '.0'
+					points += 1
+				return r + maskConverter(mask)
+			elif points == 3:
+				r  = s[0 : last] + '0'
+				return r + maskConverter(mask)
+			else:
+				last = i + 1
+				points += 1
+
 lips = []
 lmac = []
 nm = nmap.PortScanner()
 for name, interface in ifcfg.interfaces().items():
-    try:
-        if name != 'lo' :
-            netmask = unicodedata.normalize('NFKD', interface['netmask']).encode('ascii','ignore') 
-            ips = unicodedata.normalize('NFKD', interface['inet']).encode('ascii','ignore') + maskConverter(netmask)
-            interface = unicodedata.normalize('NFKD', name).encode('ascii','ignore')
-            print ("\n[*] %s" %name)
-            print ("(*) Interface: %s" % interface)
-            print ("(*) Mascara: %s" % ips)
-            conf.verb = 0 
-            ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = ips), timeout = 2, iface=interface)
-            for snd,rcv in ans:     
-                    Mac,Ip = rcv.sprintf(r"%Ether.src% %ARP.psrc%").split()
-                    if Ip not in lips:
-                        lmac.append(Mac)
-                        lips.append(Ip)
-                        nm.scan (Ip,'1-65535')
-    except:
-        pass
-print lips
+    if name != 'lo':
+   		broadcast = unicodedata.normalize('NFKD', interface['broadcast']).encode('ascii','ignore')
+
+   		if interface['netmask'] == None:
+   			netmask = broadcast2Mask(broadcast)
+		else:
+			netmask = unicodedata.normalize('NFKD', interface['netmask']).encode('ascii','ignore') 
+        
+		if interface['inet'] == None:
+			ips = broadcast2Ip(broadcast,netmask)
+		else:
+			ips = unicodedata.normalize('NFKD', interface['inet']).encode('ascii','ignore') + maskConverter(netmask)
+
+		interface = unicodedata.normalize('NFKD', name).encode('ascii','ignore')
+		print ("\n[*] %s" %name)
+		print ("(*) Interface: %s" % interface)
+		print ("(*) Mascara: %s" % ips)
+		conf.verb = 0 
+		ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = ips), timeout = 2, iface=interface)
+		for snd,rcv in ans:     
+		        Mac,Ip = rcv.sprintf(r"%Ether.src% %ARP.psrc%").split()
+		        if Ip not in lips:
+		        	lmac.append(Mac)
+		        	lips.append(Ip)
+		        	nm.scan (Ip,'1-10')
+		        	print nm.all_hosts()
+		        	print nm[Ip]
 print "\n[*] Scan Complete!" 
 
 
@@ -71,6 +118,7 @@ host = 0
 for host in nm.all_hosts():
 	print('----------------------------------------------------')
 	print('Host : %s (%s)' % (host, nm[host].hostname()))
+	print('Operational System : %s' % nm[host]['osclass'])
 	print('State : %s' % nm[host].state())
 
 	for proto in nm[host].all_protocols():
